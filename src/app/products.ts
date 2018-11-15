e8def51da7fb2dff293fe277f293d7e830b2e385
Ovarmorrow <medeconzo@gmail.com>
2018-11-14 22:06:03 -0500

    cleaned up code, setup cards and positioning

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,41 +1,16 @@
 export class products {
-  public brand ? : any;
-  public bv ? : any;
-  public cashback ? : any;
-  public ciPoints ? : any;
-  public description ? : any;
-  public ibv ? : any;
-  public id ? : String;
-  public imageUrl ? : any;
-  public links ? : any;
-  public maximumPrice ? : any;
-  public minimumPrice ? : any;
-  public name ? : String;
-  public referralUrl ? : any;
+  brand ? : any;
+  bv ? : any;
+  cashback ? : any;
+  ciPoints ? : any;
+  description ? : any;
+  ibv ? : any;
+  id ? : String;
+  imageUrl ? : any;
+  links ? : any;
+  maximumPrice ? : any;
+  minimumPrice ? : any;
+  name ? : String;
+  referralUrl ? : any;
 
-  // brand = data.brands;
-
-  // // bv = ;
-
-  // cashback = data.;
-
-  // ciPoints = ; Based on product, How Do?
-
-  // description = ;
-
-  // ibv = ;
-
-  // id ? : String;
-
-  // imageUrl = ;
-
-  // links = ;
-
-  // maximumPrice = ;
-
-  // minimumPrice = ;
-
-  // name ? : String;
-
-  // referralUrl = ;
 }
